37c9811f58a6acea5e1263682c09686680901d5b
juush <71906412+juush@users.noreply.github.com>
2021-03-11 15:34:35 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import RandomComponent from './components/RandomComponent.jsx'
+
+function App() {
+  return (
+    <RandomComponent />
+)
+}
+
+export default App;
---
+++ src/components/RandomComponent.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function RandomComponent() {
+  return (
+    <div class="bg-black text-white text-center ">
+    this is a div
+    </div>
+  );
+}
+
+export default RandomComponent;
